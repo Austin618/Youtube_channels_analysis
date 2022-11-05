5a9f13e7883f39d849934805d0455b26e9dfc6b1
Qiming Ye <1281784448yeqiming@gmail.com>
2022-11-05 10:07:26 -0400

    new search bar with multiple page routing

--- client/src/component/navbar/navbar.js
+++ client/src/component/navbar/navbar.js
@@ -3,6 +3,7 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import "./navbar.css";
 
 import axios from "axios";
+import {Link} from "react-router-dom";
 
 
 /* Component for the Home page */
@@ -11,24 +12,46 @@ class Navbar extends React.Component {
     constructor(props) {
         super(props);
         this.navRef = React.createRef();
+
+        this.state = {
+            channelId: "",
+        };
     }
 
+    handleInputChange = event => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+    }
 
     showNavbar = () => {
         this.navRef.current.classList.toggle("responsive_nav");
     };
 
     render() {
-
         return (
             <header>
-                <h1>Youtube Data Analysis</h1>
+                <Link to="/"><button className="logoBox">Youtube Analysis</button></Link>
                 <nav ref={this.navRef}>
-                    <a href="/#">Home</a>
-                    <a href="/#">Search</a>
-                    <a href="/#">Popular</a>
-                    <a href="/#">Login</a>
-                    <a href="/#">Register</a>
+                    {/*<Link to="/"><button className="buttonBox">Home</button></Link>*/}
+                    {/*<Link to="/Channel"><button className="buttonBox">Channel</button></Link>*/}
+                    <Link to="/Recommend"><button className="buttonBox">Recommend</button></Link>
+                    <input
+                        className="searchBox"
+                        placeholder=" Please enter Channel ID..."
+                        value={this.state.channelId}
+                        onChange={this.handleInputChange}
+                        name="channelId"
+                    />
+                    <Link to={`/channel/${this.state.channelId}`}>
+                        <button type="" className="buttonBox" disabled={!this.state.channelId}>
+                            Search
+                            {/*<SearchIcon />*/}
+                        </button>
+                    </Link>
                     <button
                         className="nav-btn nav-close-btn"
                         onClick={()=>{this.showNavbar()}}>
