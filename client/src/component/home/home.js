631584abb5f5d812f94aff5375dfc4e5c5dbf94b
Qiming Ye <1281784448yeqiming@gmail.com>
2022-11-06 13:19:47 -0500

    update test and instructions for using website

--- client/src/component/home/home.js
+++ client/src/component/home/home.js
@@ -79,7 +79,11 @@ class Home extends React.Component {
                 <Navbar />
                 <div className="welcome">
                     <h1>Welcome to Youtube Channel Analysis Tool.</h1>
-                    <h1>Please click type the Channel ID in the search bar to start.</h1>
+                    <h1>Please click type the Channel ID in the search bar to start.</h1><hr />
+                    <h2>For example:</h2>
+                    <h2>Google developers: UC_x5XG1OV2P6uZZ5FSM9Ttw</h2>
+                    <h2>Amazon: UCd6MoB9NC6uYN2grvUNT-Zg</h2>
+                    <h2>University of Toronto: UCWdY1GyPhp2TFpS82rFEOkQ</h2>
                 </div>
 
                 {/*<InputBase */}
--- test/testAPIs.js
+++ test/testAPIs.js
@@ -7,7 +7,7 @@ chai.should();
 
 chai.use(chaiHttp);
 
-describe('Test challengeTypes API', () => {
+describe('Test API', () => {
     describe("Test GET /api/tests", () => {
         it("GET requests should return status 200",(done)=>{
             chai.request(server)
@@ -22,35 +22,48 @@ describe('Test challengeTypes API', () => {
     })
 })
 
+describe('Test channel API', () => {
+    describe("Test GET /api/channel/:id", () => {
+        it("GET requests should return status 200 for youtube channel UC_x5XG1OV2P6uZZ5FSM9Ttw",(done)=>{
+            chai.request(server)
+                .get("/api/playlists/UC_x5XG1OV2P6uZZ5FSM9Ttw")
+                .set("Content-Type", "application/json")
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body[0].should.have.property('kind').equal("youtube#channel");
+                    response.body.should.have.length(1);
+                    done();
+                })
+        })
+    })
+})
+
 describe('Test playlists API', () => {
-    describe("Test GET /api/playlists/", () => {
-        it("GET requests should return status 200",(done)=>{
+    describe("Test GET /api/playlists/:id", () => {
+        it("GET requests should return status 200 and return 50 playlist objects for youtube channel UC_x5XG1OV2P6uZZ5FSM9Ttw",(done)=>{
             chai.request(server)
                 .get("/api/playlists/UC_x5XG1OV2P6uZZ5FSM9Ttw")
                 .set("Content-Type", "application/json")
                 .end((err, response) => {
                     response.should.have.status(200);
                     response.body[0].should.have.property('kind').equal("youtube#playlist");
+                    response.body.should.have.length(50);
                     done();
                 })
         })
+    })
+})
 
-        it("GET requests should return playlist objects",(done)=>{
+describe('Test videos API', () => {
+    describe("Test GET /api/videos/:id", () => {
+        it("GET requests should return status 200 and return 50 video objects for youtube channel UC_x5XG1OV2P6uZZ5FSM9Ttw",(done)=>{
             chai.request(server)
-                .get("/api/playlists/UC_x5XG1OV2P6uZZ5FSM9Ttw")
+                .get("/api/videos/UC_x5XG1OV2P6uZZ5FSM9Ttw")
                 .set("Content-Type", "application/json")
                 .end((err, response) => {
-                    response.body[0].should.have.property('kind').equal("youtube#playlist");
-                    done();
-                })
-        })
-
-        it("GET requests should return 50 playlist objects",(done)=>{
-            chai.request(server)
-                .get("/api/playlists/UC_x5XG1OV2P6uZZ5FSM9Ttw")
-                .set("Content-Type", "application/json")
-                .end((err, response) => {
-                    response.body.should.have.length(50)
+                    response.should.have.status(200);
+                    response.body[0].should.have.property('kind').equal("youtube#video");
+                    response.body.should.have.length(50);
                     done();
                 })
         })
